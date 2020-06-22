1e5ebb8b1b28f36c999628832a92712b22cf3e20
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-06-22 17:01:21 +0000

    getImageEffect method

--- capstone/src/main/webapp/js/image-effect-results.js
+++ capstone/src/main/webapp/js/image-effect-results.js
@@ -1,11 +1,10 @@
 window.onload = function() {
-    //alert("hello");
     //get request to get the images - get all of the images as arraylist
     fetchBlobstoreKeyframeImages();
 };
 
 function fetchBlobstoreKeyframeImages() {
-    
+
   fetch('/keyframe-image-upload', {method: 'GET'})
     .then((response) => {
         return response.text();
@@ -14,16 +13,23 @@ function fetchBlobstoreKeyframeImages() {
     
         var arrayOfKeyframeImages = JSON.parse(keyframeImages);
 
+        //we will have a URL field for the objects (in a data Java file)
+
         for (var i=0; i < arrayOfKeyframeImages.length; i++){
 
             getImageEffect(arrayOfKeyframeImages[i]);
 
         }
-
-    });
-    
+    });   
 }
 
 function getImageEffect(keyframeImage) {
-
+    fetch('/keyframe-effect-servlet', {
+        method: 'GET', 
+        image_url: keyframeImage.url
+    })
+    .then((response) => {
+        return response.text();
+    }
+    );
 }
