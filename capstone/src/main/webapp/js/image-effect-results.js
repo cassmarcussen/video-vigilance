c8cf9940a8b8138130f520a7e8e0e4f14ec63fe8
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-06-22 20:57:57 +0000

    Add image, start/end time, timestamp, and effect to the html of the results page

--- capstone/src/main/webapp/js/image-effect-results.js
+++ capstone/src/main/webapp/js/image-effect-results.js
@@ -13,18 +13,48 @@ function fetchBlobstoreKeyframeImages() {
     
         var arrayOfKeyframeImages = JSON.parse(keyframeImages);
 
-        //we will have a URL field for the objects (in a data Java file)
+        var keyframeImagesContainer = document.getElementById("results-img");
 
+        var keyframeImageDiv = document.createElement("div");
+
+        //nothing in here yet
         for (var i=0; i < arrayOfKeyframeImages.length; i++){
 
-            //how to return from this and not get promise? chaining maybe bad... fix later
-            getImageEffect(arrayOfKeyframeImages[i]).then(effect => {
-               document.getElementById("results-img").append("<p>" + effect + "</p>");
-            });
+            var thisImage = arrayOfKeyframeImages[i];
 
-            
+            var keyframeImage = document.createElement("img");
+            keyframeImage.src = thisImage.url;
 
+            if(keyframeImage.src != null && keyframeImage.src.indexOf("undefined") == -1){
+
+                //for one image
+                var singularImageDiv = document.createElement("div");
+
+                var timestamp = thisImage.timestamp;
+                var startTime = thisImage.startTime;
+                var endTime = thisImage.endTime;
+
+                var keyframeImageText = document.createElement("p");
+                keyframeImageText.innerHTML = '<p>Timestamp of image: ' + timestamp + '</p>'
+                        + '<p>Start time of frame: ' + startTime + '</p>'
+                        + '<p>End time of frame: ' + endTime + '</p>';
+
+                singularImageDiv.appendChild(keyframeImage);
+                singularImageDiv.appendChild(keyframeImageText);
+
+                var keyframeImageEffect = document.createElement("p");
+                getImageEffect(arrayOfKeyframeImages[i]).then(effect => {
+                    keyframeImageEffect.innerHTML = "<p>" + effect + "</p>";
+                });
+
+                singularImageDiv.appendChild(keyframeImageEffect);
+                keyframeImageDiv.appendChild(singularImageDiv);
+
+            }
         }
+
+        keyframeImagesContainer.append(keyframeImageDiv);
+
     });   
 }
 
