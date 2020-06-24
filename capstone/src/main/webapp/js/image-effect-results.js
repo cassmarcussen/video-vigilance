3acd548340026089adf6b30341443ac6573c5a6a
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-06-24 14:22:36 +0000

    JS for carousel of images, fix the fetching of the effect so that only one effect shows up per image, which is the effect corresponding to that image

--- capstone/src/main/webapp/js/image-effect-results.js
+++ capstone/src/main/webapp/js/image-effect-results.js
@@ -1,23 +1,39 @@
+var slideIndex = 1;
+
 window.onload = function() {
     fetchBlobstoreKeyframeImages();
 };
 
-function fetchBlobstoreKeyframeImages() {
+async function fetchBlobstoreKeyframeImages() {
 
   fetch('/keyframe-image-upload', {method: 'GET'})
     .then((response) => {
         return response.text();
     })
-    .then((keyframeImages) => {
+    .then(async function (keyframeImages) {
     
-        var arrayOfKeyframeImages = JSON.parse(keyframeImages);
+       var arrayOfKeyframeImages = JSON.parse(keyframeImages);
+
+        for (var i = 0; i < arrayOfKeyframeImages.length; i++) {
+
+            var myEffect = await getImageEffect(arrayOfKeyframeImages[i]);
+
+            arrayOfKeyframeImages[i].effect = myEffect;
+
+        }
+
+        return arrayOfKeyframeImages;
+        
+    })
+    .then((arrayOfKeyframeImages) => {
 
         var keyframeImagesContainer = document.getElementById("results-img");
 
-        var keyframeImageDiv = document.createElement("div");
-
-        //nothing in here yet
         for (var i=0; i < arrayOfKeyframeImages.length; i++){
+
+            var keyframeImageDiv = document.createElement("div");
+            keyframeImageDiv.classList.add("mySlides");
+            keyframeImageDiv.classList.add("fade");
 
             var thisImage = arrayOfKeyframeImages[i];
 
@@ -26,45 +42,52 @@ function fetchBlobstoreKeyframeImages() {
 
             if(keyframeImage.src != null && keyframeImage.src.indexOf("undefined") == -1){
 
-                var singularImageDiv = document.createElement("div");
+                keyframeImageDiv.appendChild(keyframeImage);
+
+                var imageCaptionDiv = document.createElement("div");
 
                 var timestamp = thisImage.timestamp;
                 var startTime = thisImage.startTime;
                 var endTime = thisImage.endTime;
+                var effect = thisImage.effect;
 
                 var keyframeImageText = document.createElement("p");
                 keyframeImageText.innerHTML = '<p>Timestamp of image: ' + timestamp + '</p>'
                         + '<p>Start time of frame: ' + startTime + '</p>'
-                        + '<p>End time of frame: ' + endTime + '</p>';
+                        + '<p>End time of frame: ' + endTime + '</p>'
+                          + '<p>Effect of frame: ' + effect + '</p>';
 
-                singularImageDiv.appendChild(keyframeImage);
-                singularImageDiv.appendChild(keyframeImageText);
+                imageCaptionDiv.appendChild(keyframeImageText);
 
-                var keyframeImageEffect = document.createElement("p");
-                getImageEffect(arrayOfKeyframeImages[i]).then(effect => {
-                    keyframeImageEffect.innerHTML = "<p>" + effect + "</p>";
-                });
+                keyframeImageDiv.appendChild(imageCaptionDiv);
 
-                singularImageDiv.appendChild(keyframeImageEffect);
-                keyframeImageDiv.appendChild(singularImageDiv);
+                keyframeImagesContainer.append(keyframeImageDiv);
 
+                // Make the first image display, and first dot highlighted
+                if (i == 0) {
+                    keyframeImageDiv.style.display = "block";
+                    document.getElementById("dots").innerHTML += '<span class="dot active" onclick="currentSlide(' + (i + 1) + ')"></span>';
+                } else {
+                    document.getElementById("dots").innerHTML += '<span class="dot" onclick="currentSlide(' + (i + 1) + ')"></span>';
+                }
             }
-        }
 
-        keyframeImagesContainer.append(keyframeImageDiv);
+        }
 
     });   
 }
 
 function getImageEffect(keyframeImage) {
-    var response = fetch('/keyframe-effect-servlet', {
-        method: 'GET', 
-        image_url: keyframeImage.url
-    })
+
+    var response = fetch('/keyframe-effect-servlet?image_url=' + keyframeImage.url, 
+        {
+            method: 'GET'
+        })
     .then((response) => {
         return response.text();
     })
     .then((response) => {
+
         return response;
     }
     );
@@ -72,3 +95,32 @@ function getImageEffect(keyframeImage) {
     return response;
 
 }
+
+/* Based on https://www.w3schools.com/howto/howto_js_slideshow.asp */
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
