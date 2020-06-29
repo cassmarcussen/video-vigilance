7eafe709879287337ac38d659f357590ee5ab564
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-06-29 16:06:12 +0000

    Detect SafeSearch - return results based on Cloud Vision output

--- capstone/src/main/java/com/google/sps/servlets/DetectSafeSearchGcs.java
+++ capstone/src/main/java/com/google/sps/servlets/DetectSafeSearchGcs.java
@@ -1,6 +1,6 @@
 package com.google.sps.servlets;
 
-/*import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageRequest;
 import com.google.cloud.vision.v1.AnnotateImageResponse;
 import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
 import com.google.cloud.vision.v1.Feature;
@@ -8,7 +8,7 @@ import com.google.cloud.vision.v1.Feature.Type;
 import com.google.cloud.vision.v1.Image;
 import com.google.cloud.vision.v1.ImageAnnotatorClient;
 import com.google.cloud.vision.v1.ImageSource;
-import com.google.cloud.vision.v1.SafeSearchAnnotation;*/
+import com.google.cloud.vision.v1.SafeSearchAnnotation;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,7 +27,7 @@ public class DetectSafeSearchGcs {
 
     System.out.println("minor change to see if push works");
     HashMap<String, String> safeSearchResults = new HashMap<String, String>();
-    /*List<AnnotateImageRequest> requests = new ArrayList<>();
+    List<AnnotateImageRequest> requests = new ArrayList<>();
 
     ImageSource imgSource = ImageSource.newBuilder().setGcsImageUri(gcsPath).build();
     Image img = Image.newBuilder().setSource(imgSource).build();
@@ -36,7 +36,7 @@ public class DetectSafeSearchGcs {
         AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
     requests.add(request);
 
-    String outputResult;
+   // String outputResult;
 
     // Initialize client that will be used to send requests. This client only needs to be created
     // once, and can be reused for multiple requests. After completing all of your requests, call
@@ -48,35 +48,40 @@ public class DetectSafeSearchGcs {
       for (AnnotateImageResponse res : responses) {
         if (res.hasError()) {
           System.out.format("Error: %s%n", res.getError().getMessage());
-          return;
+          safeSearchResults.put("adult", res.getError().getMessage());
+          safeSearchResults.put("medical", "UNKNOWN");
+          safeSearchResults.put("spoofed", "UNKNOWN");
+          safeSearchResults.put("violence", "UNKNOWN");
+          safeSearchResults.put("racy", "UNKNOWN");
+          return safeSearchResults;
         }
 
         // For full list of available annotations, see http://g.co/cloud/vision/docs
         SafeSearchAnnotation annotation = res.getSafeSearchAnnotation();
-        outputResult = String.format(
+      /*  outputResult = String.format(
             "adult: %s%nmedical: %s%nspoofed: %s%nviolence: %s%nracy: %s%n",
             annotation.getAdult(),
             annotation.getMedical(),
             annotation.getSpoof(),
             annotation.getViolence(),
-            annotation.getRacy());
+            annotation.getRacy());*/
 
-        safeSearchResults.put("adult", annotation.getAdult());
-        safeSearchResults.put("medical", annotation.getMedical());
-        safeSearchResults.put("spoofed", annotation.getSpoof());
-        safeSearchResults.put("violence", annotation.getViolence());
-        safeSearchResults.put("racy", annotation.getRacy());
+        safeSearchResults.put("adult", annotation.getAdult().toString());
+        safeSearchResults.put("medical", annotation.getMedical().toString());
+        safeSearchResults.put("spoofed", annotation.getSpoof().toString());
+        safeSearchResults.put("violence", annotation.getViolence().toString());
+        safeSearchResults.put("racy", annotation.getRacy().toString());
         
       }
     }
 
-    //return outputResult;*/
+    //return outputResult;
 
-    safeSearchResults.put("adult", "VERY_LIKELY");
+   /* safeSearchResults.put("adult", "VERY_LIKELY");
     safeSearchResults.put("medical", "VERY_UNLIKELY");
     safeSearchResults.put("spoofed", "UNLIKELY");
     safeSearchResults.put("violence", "POSSIBLE");
-    safeSearchResults.put("racy", "LIKELY");
+    safeSearchResults.put("racy", "LIKELY");*/
 
     return safeSearchResults;
     //return gcsPath + " adult: medical: spoofed: violence: racy: ";
