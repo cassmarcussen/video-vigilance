0d0bea5dc7d86c386d6a68258d7fe24746bd993a
Sharon <sharondo@seas.upenn.edu>
2020-06-30 13:49:34 +0000

    delete unused code in js file

--- capstone/src/main/webapp/js/extract-images-script.js
+++ capstone/src/main/webapp/js/extract-images-script.js
@@ -12,23 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function uploadFile() {
-var bucket = document.forms["putFile"]["bucket"].value;
-        var filename = document.forms["putFile"]["fileName"].value;
-        if (bucket == null || bucket == "" || filename == null || filename == "") {
-          alert("Both Bucket and FileName are required");
-          return false;
-        } else {
-          var postData = document.forms["putFile"]["content"].value;
-          document.getElementById("content").value = null;
-
-          var request = new XMLHttpRequest();
-          request.open("POST", "/gcs/" + bucket + "/" + filename, false);
-          request.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
-          request.send(postData);
-        }
-}
-
 function upload() {
   fetch("/video");
 }
@@ -41,7 +24,7 @@ function time() {
 
 function play() {
   var video = document.getElementById("video");
-  this.currentTime = 5;
+  video.play();
 }
 
 function pause() {
@@ -50,56 +33,45 @@ function pause() {
 }
 
 function loadVideo() {
-    // const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const url = "https://storage.cloud.google.com/video-vigilance-bucket/Video%20Of%20Flower%20Field.mp4?organizationId=433637338589"; // site that doesn’t send Access-Control-*
-    // fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
-    // .then(response => response.text())
-    // .then(contents => console.log(contents))
-    // .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
-    //console.log(fetch("storage.googleapis.com/video-vigilance-bucket"));
-
-    //video.crossOrigin = "Anonymous";
-    video.src = URL.createObjectURL(document.querySelector("#vid").files[0]);
-    // video.src = url;
+  video.src = URL.createObjectURL(document.querySelector("#vid").files[0]);
 }
 
 // Captures an image from the video and draws it onto the html page
 function capture() {
-    var canvas = document.getElementById('canvas');     
-    var video = document.getElementById('video');
-    // var video = document.getElementById("vid").value;
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
+  var canvas = document.getElementById('canvas');
+  var video = document.getElementById('video');
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
 
-    // Get 2d drawing context on canvas
-    var ctx = canvas.getContext("2d");
-    
-    // Draw video's current screen as an image onto canvas
-    // 0, 0 sets the top left corner of where to start drawing
-    // video.videoWidth, vidoe.videoHeight allows proper scaling when drawing the image
-    ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);  
+  // Get 2d drawing context on canvas
+  var ctx = canvas.getContext("2d");
 
-    // Create a Blob object representing the image contained in "canvas"
-    // Can specify image format: https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob
-    
-    canvas.toBlob(saveFrame);
+  // Draw video's current screen as an image onto canvas
+  // 0, 0 sets the top left corner of where to start drawing
+  // video.videoWidth, vidoe.videoHeight allows proper scaling when drawing the image
+  ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
 
-    // toDataURL creates a huuuugggeee url
-    // var dataURL = canvas.toDataURL('image/png');
-    // var newImg = document.createElement('img');
-    // newImg.src = dataURL;
-    // console.log("image source: " + newImg.src);
-    // document.body.appendChild(newImg);
+  // Create a Blob object representing the image contained in "canvas"
+  // Can specify image format: https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob
+
+  canvas.toBlob(saveFrame);
+
+  // toDataURL creates a huuuugggeee url
+  // var dataURL = canvas.toDataURL('image/png');
+  // var newImg = document.createElement('img');
+  // newImg.src = dataURL;
+  // console.log("image source: " + newImg.src);
+  // document.body.appendChild(newImg);
 }
 
 function saveFrame(blob) {
   var newImg = document.createElement('img');
   var url = URL.createObjectURL(blob);
 
-// newImg.onload = function() {
-//     // no longer need to read the blob so it's revoked
-//     URL.revokeObjectURL(url);
-//   };
+  // newImg.onload = function() {
+  //     // no longer need to read the blob so it's revoked
+  //     URL.revokeObjectURL(url);
+  //   };
 
   newImg.src = url;
   console.log("image source: " + newImg.src);
