a4dde648637a25c6dc9d9a6f0b040b5bc1273e71
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-07-01 15:45:01 +0000

    Store image's GCS URL in DataStore, and sort by timeframe, so that images, their effect, and timestamp all show up correspondingly

--- capstone/src/main/java/com/google/sps/servlets/KeyframeImageUploadServlet.java
+++ capstone/src/main/java/com/google/sps/servlets/KeyframeImageUploadServlet.java
@@ -1,15 +1,8 @@
 package com.google.sps.servlets;
 
-import com.google.api.gax.paging.Page;
-import com.google.cloud.storage.Blob;
-import com.google.cloud.storage.Bucket;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
-import com.google.appengine.tools.cloudstorage.GcsFilename;
-
 import com.google.appengine.api.blobstore.BlobInfo;
 import com.google.appengine.api.blobstore.BlobInfoFactory;
-import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobKey;  
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import com.google.appengine.api.datastore.DatastoreService;
@@ -46,24 +39,18 @@ public class KeyframeImageUploadServlet extends HttpServlet {
     List<KeyframeImage> keyframeImagesFromVideo = new ArrayList<>();
 
     Query query = new Query("KeyframeImages_Video");
-    /*query.addSort("timestamp",
-                     Query.SortDirection.ASCENDING);*/
+    query.addSort("timestamp",
+                     Query.SortDirection.ASCENDING);
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
 
-    ArrayList<String> blobNameList = listObjects();
-    int index = 0;
-
     for (Entity entity : results.asIterable()) {
 
-      String blobUrl = blobNameList.get(index);
-      index++;
+      String urlForGCS = (String) entity.getProperty("url");
 
-      //String url = "/serve?blobkey=" + (String) entity.getProperty("url");
-      String url = "gs://keyframe-images-to-effect/" + blobUrl;
-     // String url = "gs://keyframe-images-to-effect/" + (String) entity.getKey().toString();
-      //String url = "gs://keyframe-images-to-effect/AAANsUnEN0kk0CgoIhTIMc-n9MP58-yXINKxCbJvRy-40UQbYnZzBvV3P0SebWrDKw9QP_sSsAvoCWz89fmGzjR2L80dOgmvTA.AtjA0-T3C1vf4LGu";
+      String url = "gs:/" + urlForGCS;
+
       String timestamp = (String) entity.getProperty("timestamp");
       long id = entity.getKey().getId();
       String startTime = (String) entity.getProperty("startTime");
@@ -73,41 +60,17 @@ public class KeyframeImageUploadServlet extends HttpServlet {
 
       keyframeImagesFromVideo.add(img);
 
-     // }
-    
     }
 
     //sort keyframe images from video by timestamp now, after URL retrieved
    // keyframeImagesFromVideo = sortKeyframeImagesByTimestamp(keyframeImagesFromVideo);
-    Collections.sort(keyframeImagesFromVideo);
+    //Collections.sort(keyframeImagesFromVideo);
 
     response.setContentType("application/json;");
     response.getWriter().println(gson.toJson(keyframeImagesFromVideo));
  
   }
   
-  // static because used as helper method in this file as well as DeleteDataServlet
-  public static ArrayList<String> listObjects() {
-    // The ID of your GCP project
-    String projectId = "video-vigilance";
-
-    // The ID of your GCS bucket
-    String bucketName = "keyframe-images-to-effect";
-
-    Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
-    Bucket bucket = storage.get(bucketName);
-    Page<Blob> blobs = bucket.list();
-
-    ArrayList<String> blobNameList = new ArrayList<String>();
-
-    for (Blob blob : blobs.iterateAll()) {
-      System.out.println(blob.getName());
-      blobNameList.add(blob.getName());
-    }
-
-    return blobNameList;
-  }
-
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
@@ -145,6 +108,7 @@ public class KeyframeImageUploadServlet extends HttpServlet {
   private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName) {
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
     Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
+
     List<BlobKey> blobKeys = blobs.get("image");
 
     // User submitted form without selecting a file, so we can't get a URL. (dev server)
@@ -153,7 +117,11 @@ public class KeyframeImageUploadServlet extends HttpServlet {
     }
 
     BlobKey blobKey = blobKeys.get(0);
-    return blobKey.getKeyString();
+
+    BlobInfo info = new BlobInfoFactory().loadBlobInfo(blobKey);
+    String gcsName = info.getGsObjectName();
+
+    return gcsName;
     
   }
 
