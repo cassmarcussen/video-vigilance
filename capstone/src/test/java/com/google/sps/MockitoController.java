27c3461ec80d87f64ebe0b0c0ec51c188fb46c32
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-07-07 17:19:11 +0000

    Call Upload Servlet's Get and Post using MockHttpServletRequest/Response

--- capstone/src/test/java/com/google/sps/MockitoController.java
+++ capstone/src/test/java/com/google/sps/MockitoController.java
@@ -1,4 +1,4 @@
-package com.google.sps;
+package com.google.sps.servlets;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -7,17 +7,45 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.io.IOException;
 
 import java.time.LocalDateTime;
 
 @Controller
 public class MockitoController {
 
+    private KeyframeImageUploadServlet uploadServlet;
+
     @RequestMapping(value = "/keyframe-image-upload", method = RequestMethod.POST)
-    public String post() {
+    public String post() throws IOException {
+        
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        request.addParameter("", "");
+        request.addParameter("", "");
+        request.addParameter("", "");
+        request.addParameter("", "");
+
+        uploadServlet.doPost(request, response);
 
         System.out.println("POSTING...");
-        return "index";
+        return "post...";
+    }
+
+    @RequestMapping(value = "/keyframe-image-upload", method = RequestMethod.GET)
+    public String get() throws IOException {
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        uploadServlet.doGet(request, response);
+
+        System.out.println("GET...");
+        return "get...";
     }
 
 }
