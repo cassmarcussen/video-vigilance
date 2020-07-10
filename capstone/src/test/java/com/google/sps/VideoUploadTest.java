ac258e70d43fc77e11b66d2de97f61ccfe84b1ad
Sharon <sharondo@seas.upenn.edu>
2020-07-10 17:35:39 +0000

    add pom dependencies

--- capstone/src/test/java/com/google/sps/VideoUploadTest.java
+++ capstone/src/test/java/com/google/sps/VideoUploadTest.java
@@ -14,17 +14,11 @@
 
 package com.google.sps;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
 
 @RunWith(JUnit4.class)
 public final class VideoUploadTest {
