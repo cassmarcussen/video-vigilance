51e62671f5bfb5a9086d9c76e34805bdd2863f0a
Sharon <sharondo@seas.upenn.edu>
2020-07-10 17:47:32 +0000

    fill in VideoUploadTest

--- capstone/src/test/java/com/google/sps/VideoUploadTest.java
+++ capstone/src/test/java/com/google/sps/VideoUploadTest.java
@@ -14,14 +14,54 @@
 
 package com.google.sps;
 
-import org.junit.Assert;
+import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
+import static org.junit.Assert.assertEquals;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
-@RunWith(JUnit4.class)
-public final class VideoUploadTest {
-
+/** Datastore tests for uploading the video
+  * Taken from: https://cloud.google.com/appengine/docs/standard/java/tools/localunittesting?csw=1#datastore-memcache
+  */
+public class VideoUploadTestTest {
   
+  // Configures the local datastore service to keep all data in memory
+  private final LocalServiceTestHelper helper =
+      new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+
+  @Before
+  public void setUp() {
+    helper.setUp();
+  }
+
+  @After
+  public void tearDown() {
+    helper.tearDown();
+  }
+
+  // Run this test twice to prove we're not leaking any state across tests.
+  private void doTest() {
+    DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+    assertEquals(0, ds.prepare(new Query("yam")).countEntities(withLimit(10)));
+    ds.put(new Entity("yam"));
+    ds.put(new Entity("yam"));
+    assertEquals(2, ds.prepare(new Query("yam")).countEntities(withLimit(10)));
+  }
+
+  @Test
+  public void testInsert1() {
+    doTest();
+  }
+
+  @Test
+  public void testInsert2() {
+    doTest();
+  }
 }
