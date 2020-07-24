912f963e79f66a161249ab84bb2d23f5119e904d
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-07-24 14:24:13 +0000

    Remove printing of error

--- capstone/src/main/java/com/google/sps/servlets/DetectSafeSearchGcs.java
+++ capstone/src/main/java/com/google/sps/servlets/DetectSafeSearchGcs.java
@@ -48,7 +48,6 @@ public class DetectSafeSearchGcs {
 
       for (AnnotateImageResponse res : responses) {
         if (res.hasError()) {
-          System.out.format("Error: %s%n", res.getError().getMessage());
           safeSearchResults.put("adult", "UNKNOWN");
           safeSearchResults.put("medical", "UNKNOWN");
           safeSearchResults.put("spoofed", "UNKNOWN");
