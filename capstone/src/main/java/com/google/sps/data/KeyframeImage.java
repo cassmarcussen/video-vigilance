07855800606dde2dee26804fe1e37e796135bdcb
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-07-27 21:06:21 +0000

    Update a comment in the code

--- capstone/src/main/java/com/google/sps/data/KeyframeImage.java
+++ capstone/src/main/java/com/google/sps/data/KeyframeImage.java
@@ -8,7 +8,7 @@ This class implements Comparable, so that it can have a compareTo method which c
 public class KeyframeImage {
 
   private String cloudBucketUrl;
-  // The following three int variables are times in number of seconds.
+  // The following integer variables is a time in number of seconds.
   private int timestamp;
   private String safeSearchEffect;
 
