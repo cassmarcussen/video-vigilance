08a850e7ee493da5509c9fa9324ef95974db074b
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-07-29 17:25:54 +0000

    compareTo to sort Keyframe Images in the GET upload method

--- capstone/src/main/java/com/google/sps/data/KeyframeImage.java
+++ capstone/src/main/java/com/google/sps/data/KeyframeImage.java
@@ -7,7 +7,7 @@ Google Cloud Bucket URL of the image, the timestamp of the image, the start and 
 effect of the image, which is filled in from the results of the Vision API on the keyframe image. 
 This class implements Comparable, so that it can have a compareTo method which can be used for sorting the KeyframeImages by timestamp.
 */
-public class KeyframeImage {
+public class KeyframeImage implements Comparable<KeyframeImage>{
 
   private String cloudBucketUrl;
   // The following integer variables is a time in number of seconds.
@@ -42,5 +42,20 @@ public class KeyframeImage {
 
   public HashMap<String, String> getEffect() {
     return safeSearchEffect;
+  }
+
+  // Implement compareTo for Keyframe Images because we want to return the keyframe images in the 
+  // order of increasing timestamps. DataStore queries order timestamps alphabetically, prioritizing 
+  // the value of the highest order digit, which does not return a numeric ordering which is accurate, 
+  // so we must implement this method to compare the Keyframe Images.
+  @Override
+  public int compareTo(KeyframeImage other) {
+    if (timestamp < other.timestamp) {
+        return -1;
+    } else if (timestamp == other.timestamp) {
+        return 0;
+    } else {
+        return 1;
+    }
   }
 }
