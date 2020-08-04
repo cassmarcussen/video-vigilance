139855450b2a06ef5ed12673a71d387058dfba75
Rhiannan Wackes <wackesrhiannan@gmail.com>
2020-08-04 22:25:41 +0000

    Update class comment to reflect that this test class tests PerspectiveAPI and PerspectiveAPIBuilder.

--- capstone/src/test/java/com/google/sps/PerspectiveAPITest.java
+++ capstone/src/test/java/com/google/sps/PerspectiveAPITest.java
@@ -29,7 +29,7 @@ import org.junit.runners.JUnit4;
 
 import static org.mockito.Mockito.*;
 
-/** Unit Test class for PerspectiveAPI*/
+/** Unit Test class for PerspectiveAPI and PerspectiveAPIBuilder*/
 @RunWith(JUnit4.class)
 public class PerspectiveAPITest {
 
